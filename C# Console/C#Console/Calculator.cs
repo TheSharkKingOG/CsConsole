82095b5e66a226f3e216d9c4272eb7a899887ca3
Christopher Yarberry <27yarberrycd@MISDTX.AD.K12>
2024-09-24 09:23:25 -0500

    Create Calculator.cs

---
+++ C# Console/C#Console/Calculator.cs
@@ -0,0 +1,22 @@
+namespace MyCalculatorApp
+{
+    public class myCalculatorApp1
+    {
+       public int Add(int a, int b)
+       {
+           return a + b;
+       }  
+       public int Add(int a, int b, int c)
+       {
+        return a + b + c;
+       }
+       public double Add(double a, double b)
+       {
+        return a + b;
+       }
+    
+    
+    
+    
+    }
+}
